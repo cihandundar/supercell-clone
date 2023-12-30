976b0e576b965886ea0b7c7adbd342bd060a124b
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-12-31 00:35:52 +0300

    slider function added

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const searchIcon = document.getElementById("search");
-  const inputElement = document.getElementById("input");
-
-  searchIcon.addEventListener("click", function () {
-    inputElement.classList.toggle("hidden");
-    inputElement.style.top = "30px";
+const initSlider = () => {
+  const slideBox = document.querySelector(
+    ".section-slider-container .section-slider-conmtainer-wrapper"
+  );
+  const slideButtons = document.querySelectorAll(
+    ".section-slider-container .slide-button"
+  );
+  const maxScrollLeft = slideBox.scrollWidth - slideBox.clientWidth;
+  slideButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const direction = button.id === `prev-slide` ? -1 : 1;
+      const scrollAmount = slideBox.clientWidth * direction;
+      slideBox.scrollBy({ left: scrollAmount, behavior: "smooth" });
+    });
   });
-});
+
+  const handleSlideButton = () => {
+    slideButtons[0].style.display = slideBox.scrollLeft <= 0 ? "none" : "block";
+    slideButtons[1].style.display =
+      slideBox.scrollLeft >= maxScrollLeft ? "none" : "block";
+  };
+
+  slideBox.addEventListener("scroll", () => {
+    handleSlideButton();
+  });
+};
+
+window.addEventListener("load", initSlider);
